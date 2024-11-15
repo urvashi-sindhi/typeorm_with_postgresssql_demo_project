2f70b2113f2484c6d705540cd4e6aac4a66ba5dd
urvashi17 <urvashi.shivinfotech@gmail.com>
2024-11-15 14:31:14 +0530

    creating list of service, get service, view service api

--- src/service/service.controller.ts
+++ src/service/service.controller.ts
@@ -2,16 +2,18 @@ import {
   Body,
   Controller,
   Delete,
+  Get,
   HttpCode,
   HttpStatus,
   Param,
   Post,
   Put,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { ServiceService } from './service.service';
 import { JwtGuard } from 'src/lib/services/auth/guard/jwt.guard';
-import { ApiBearerAuth, ApiParam, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { ServiceDto } from './dto/service.dto';
 import { ApiTag } from 'src/lib/utils/enum';
 
@@ -44,5 +46,64 @@ export class ServiceController {
   @Delete('service/deleteService/:serviceId')
   deleteService(@Param('serviceId') serviceId: number) {
     return this.serviceService.deleteService(serviceId);
+  }
+
+  @ApiQuery({
+    name: 'sortKey',
+    type: String,
+    required: false,
+  })
+  @ApiQuery({
+    name: 'sortValue',
+    type: String,
+    enum: ['asc', 'desc'],
+    required: false,
+  })
+  @ApiQuery({
+    name: 'pageSize',
+    type: 'number',
+    required: false,
+  })
+  @ApiQuery({
+    name: 'page',
+    type: 'number',
+    required: false,
+  })
+  @ApiQuery({
+    name: 'searchBar',
+    type: 'string',
+    required: false,
+  })
+  @UseGuards(JwtGuard)
+  @ApiBearerAuth()
+  @HttpCode(HttpStatus.OK)
+  @Get('service/listOfService')
+  listOfService(
+    @Query('sortKey') sortKey: string,
+    @Query('sortValue') sortValue: string,
+    @Query('pageSize') pageSize: number,
+    @Query('page') page: number,
+    @Query('searchBar') searchBar: string,
+  ) {
+    return this.serviceService.listOfService({
+      sortValue,
+      sortKey,
+      pageSize,
+      page,
+      searchBar,
+    });
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get('customer/getServiceList')
+  getServiceList() {
+    return this.serviceService.getServiceList();
+  }
+
+  @ApiParam({ example: 1, name: 'serviceId', required: true })
+  @HttpCode(HttpStatus.OK)
+  @Get('customer/viewService/:serviceId')
+  viewService(@Param('serviceId') serviceId: number) {
+    return this.serviceService.viewService(serviceId);
   }
 }
